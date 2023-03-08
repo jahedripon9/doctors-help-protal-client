cbcf955d001905d4af7c0ea24dd97a725c72e57a
jahedripon9 <jahedahmed3170@gmail.com>
2023-03-08 21:52:57 +0600

    Service Modal

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm();
+    
+    const handleLogin = data => {
+    console.log(data);
+}
+
     return (
-        <div className="mockup-code">
-            <pre data-prefix="$"><code>npm i daisyui</code></pre>
+        <div className="h-[800px] flex justify-center items-center">
+            <div className='w-96 p-6'>
+                <h2 className='text-xl text-center font-bold'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    <div className="form-control w-full ">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input {...register("email")} className="input input-bordered w-full " />
+                    </div>
+                    <div className="form-control w-full ">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type='password' {...register("password")} className="input input-bordered w-full " />
+                        <span className="label-text">Forget Password?</span>
+
+                    </div>
+                    <input className='btn btn-accent w-full' value='Login' type="submit" />
+                </form>
+                <p className='text-sm'>New to Doctors Help Portal <Link className='text-secondary' to='/signup'>Create new account</Link></p>
+                <div className="divider">OR</div>
+                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+            </div>
         </div>
 
     );
